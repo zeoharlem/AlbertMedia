cc3077a8aeda903fec34c425c8eebaf45e99491f
Theophilus <zeoharlem@yahoo.co.uk>
2020-06-03 09:23:10 +0100

    Initial commit

---
+++ app/src/main/java/com/zeoharlem/albert/alphateamone/WelcomeIntroActivity.java
@@ -0,0 +1,14 @@
+package com.zeoharlem.albert.alphateamone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_intro);
+    }
+}
